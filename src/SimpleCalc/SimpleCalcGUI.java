9b2a7acadaa07213bc3cf13758db946479c6771f
Erwin H. Lambujon <erwin.lambujon@cit.edu>
2022-12-17 13:05:45 +0800

    Rename SimpleCalculator components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,8 +3,11 @@ package SimpleCalc;
 import javax.swing.*;
 
 public class SimpleCalcGUI {
+    JFrame frame = new JFrame("Simple Calculator");
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
 }
