33f14732e5d6904fb1b5d5693239006cc2ee519e
Erwin H. Lambujon <erwin.lambujon@cit.edu>
2022-12-17 13:13:14 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,6 +1,8 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame{
     JFrame frame = new JFrame("Leap Year Checker");
@@ -8,6 +10,15 @@ public class LeapYearGUI extends JFrame{
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                LeapYearChecker();
+            }
+        });
+    }
     public void LeapYearChecker(){
             int year = Integer.parseInt(tfYear.getText());
             if (year % 4 == 0 && year % 100 != 0) {
@@ -19,6 +30,9 @@ public class LeapYearGUI extends JFrame{
             }
     }
     public static void main(String[] args){
-
+        LeapYearGUI leap = new LeapYearGUI();
+        leap.setContentPane(leap.panel1);
+        leap.setSize(200, 300);
+        leap.setVisible(true);
     }
 }
