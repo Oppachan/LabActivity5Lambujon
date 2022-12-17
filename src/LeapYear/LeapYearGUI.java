34853fbf2a5246ce9cb0b849f8e64a36d779cad4
Erwin H. Lambujon <erwin.lambujon@cit.edu>
2022-12-17 12:55:12 +0800

    Rename LeapYear components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -3,7 +3,19 @@ package LeapYear;
 import javax.swing.*;
 
 public class LeapYearGUI {
+    JFrame frame = new JFrame("Leap Year Checker");
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+    public void LeapYearChecker(){
+            int year = Integer.parseInt(tfYear.getText());
+            if (year % 4 == 0 && year % 100 != 0) {
+                JOptionPane.showMessageDialog(panel1, "Leap Year");
+            }else if(year % 400 == 0){
+                JOptionPane.showMessageDialog(panel1, "Leap Year");
+            }else{
+                JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+            }
+    }
 }
